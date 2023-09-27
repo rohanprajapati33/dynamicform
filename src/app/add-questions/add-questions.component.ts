dd793605e88b00c12b36a55b38238ada132f9b3b
rohanprajapati33 <rohanp.albiorix@gmail.com>
2023-09-27 19:03:40 +0530

    feat(dynamic-form):Implement add update and delete functionality

--- src/app/add-questions/add-questions.component.ts
+++ src/app/add-questions/add-questions.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-add-questions',
@@ -8,12 +9,16 @@ import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class AddQuestionsComponent {
   addQuestionsForm!: FormGroup;
-  addQuestionsFormArray: Array<string> = [];
+  addQuestionsFormArray: any[] = [];
   isEditDetails: boolean = false;
-  editIndex: number | null = null;
-  questionToEdit: any;
+  editIndex!: number;
+  questionToEdit!: any;
+  formName: string = '';
 
-  constructor(private formbuilder: FormBuilder) {}
+  constructor(
+    private formbuilder: FormBuilder,
+    private route: ActivatedRoute
+  ) {}
 
   typeList: string[] = [
     'text',
@@ -30,6 +35,10 @@ export class AddQuestionsComponent {
     return this.addQuestionsForm.get('option') as FormArray;
   }
 
+  get validationsControl() {
+    return this.addQuestionsForm.get('validations') as FormArray;
+  }
+
   ngOnInit() {
     this.addQuestionsForm = this.formbuilder.group({
       questions: ['', Validators.required],
@@ -41,31 +50,25 @@ export class AddQuestionsComponent {
       minValue: [''],
       maxValue: [''],
     });
+    this.showQuestions();
+    // this.formName = this.route.snapshot.paramMap.get('formName') || '';
   }
 
   addQuestions() {
-    // if (this.addQuestionsForm.valid) {
-    //   this.addQuestionsFormArray.push(
-    //     this.addQuestionsForm.get('questions')?.value
-    //   );
-    // }
-    // this.saveToLocalstorage();
-    // this.addQuestionsForm.reset();
-
     if (this.addQuestionsForm.valid) {
       if (this.isEditDetails) {
-        this.addQuestionsFormArray[this.editIndex!] =
+        this.addQuestionsFormArray[this.editIndex] =
           this.addQuestionsForm.value;
-        this.isEditDetails = true;
       } else {
         this.addQuestionsFormArray.push(this.addQuestionsForm.value);
       }
       this.saveToLocalstorage();
       this.addQuestionsForm.reset();
     }
+    console.log(this.addQuestionsFormArray);
   }
 
-  formGroup() {
+  optionFormGroup() {
     return this.formbuilder.group({
       option1: [''],
       option2: [''],
@@ -74,9 +77,15 @@ export class AddQuestionsComponent {
 
   removeQuestions(index: number) {
     this.addQuestionsFormArray.splice(index, 1);
+    localStorage.setItem(
+      'add-questions',
+      JSON.stringify(this.addQuestionsFormArray)
+    );
   }
 
   editQuestions(index: number) {
+    this.isEditDetails = true;
+    this.editIndex = index;
     this.questionToEdit = this.addQuestionsFormArray[index];
     this.addQuestionsForm.patchValue(this.questionToEdit);
   }
@@ -85,11 +94,23 @@ export class AddQuestionsComponent {
     if (this.addQuestionsForm.invalid) return;
     const storedQueData =
       JSON.parse(localStorage.getItem('add-questions') as '[]') || [];
-    storedQueData.push(this.addQuestionsForm.value);
+    if (this.isEditDetails) {
+      storedQueData[this.editIndex] = this.addQuestionsForm.value;
+      this.isEditDetails = false;
+    } else {
+      storedQueData.push(this.addQuestionsForm.value);
+    }
     localStorage.setItem('add-questions', JSON.stringify(storedQueData));
   }
 
+  showQuestions() {
+    const storedQueData = JSON.parse(
+      localStorage.getItem('add-questions') || '[]'
+    );
+    this.addQuestionsFormArray = storedQueData;
+  }
+
   addOption() {
-    this.optionArray.push(this.formGroup());
+    this.optionArray.push(this.optionFormGroup());
   }
 }
